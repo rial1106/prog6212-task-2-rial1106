913faf3f44757aadad998ea5d1aa07fb50fc724a
Rial Seebran <ST10083702@vcconnect.edu.za>
2022-11-02 17:25:34 +0200

    Import Code from Task 1 Repo

---
+++ Models/User.cs
@@ -0,0 +1,41 @@
+﻿namespace PROG6212.Models
+{
+    /* The User class hold the username and hashed password for the current user.
+     * The class is static as we will only be needing one throughout the runtime of the program.
+     */
+    internal static class User
+    {
+
+        private static string Username; // The logged in Username.
+        private static string Password; // How many weeks long the semester is.
+
+        // Getters and Setters.
+        public static string Username1
+        {
+            get
+            {
+                return Username;
+            }
+
+            set
+            {
+                Username = value;
+            }
+        }
+
+        public static int Password1
+        {
+            get
+            {
+                return Password;
+            }
+
+            set
+            {
+                Password = value;
+            }
+        }
+
+
+    }
+}
